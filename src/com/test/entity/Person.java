4bb4006a64b9623fc536d441e5cc7026718b6460
renzhiming <649136934@qq.com>
2017-11-06 22:30:49 +0800

    提交

--- src/com/test/entity/Person.java
+++ src/com/test/entity/Person.java
@@ -3,7 +3,7 @@ package com.test.entity;
 public class Person {
 
 	private String id;
-	private Integer age;
+//	private Integer age;
 
 	public String getId() {
 		return id;
@@ -13,12 +13,12 @@ public class Person {
 		this.id = id;
 	}
 
-	public Integer getAge() {
-		return age;
-	}
-
-	public void setAge(Integer age) {
-		this.age = age;
-	}
+//	public Integer getAge() {
+//		return age;
+//	}
+//
+//	public void setAge(Integer age) {
+//		this.age = age;
+//	}
 
 }
